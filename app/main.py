f935ec930f2ed8807900b49bc30be35174a7f04d
Carl Nakad <c_nakad@live.concordia.ca>
2024-10-12 13:14:39 -0400

    Uncomment MongoDB api

--- app/main.py
+++ app/main.py
@@ -9,12 +9,12 @@ app = FastAPI()
 async def root():
     return {"Hello": "World!"}
 
-# @app.get("/user/{user_id}")
-# async def get_data(user_id: str):
-#     reviews_collection = db.reviews
-#     user_object_id = ObjectId(user_id)
-#     user = reviews_collection.find_one({"_id": user_object_id})
-#     if user:
-#         user['_id'] = str(user['_id']) 
-#         return user
-#     return {"error": "User not found"}
+@app.get("/user/{user_id}")
+async def get_data(user_id: str):
+    reviews_collection = db.reviews
+    user_object_id = ObjectId(user_id)
+    user = reviews_collection.find_one({"_id": user_object_id})
+    if user:
+        user['_id'] = str(user['_id']) 
+        return user
+    return {"error": "User not found"}
