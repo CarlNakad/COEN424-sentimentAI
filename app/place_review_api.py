e64f301b9e1884d5ab87b07a8476a14b6ad5d702
emkay21 <ma_kacho@live.concordia.ca>
2024-11-01 15:13:29 -0400

    Refactor to remove the old records and insert the new ones (not complete)

--- app/place_review_api.py
+++ app/place_review_api.py
@@ -4,6 +4,14 @@ from foursquare_api import get_foursquare_place, get_foursquare_place_reviews
 from google_nlp import analyze_sentiment
 from mongodb_connection import db
 
+def have_reviews_changed(place_id, new_reviews_id):
+    existing_review_id = db['review'].find({"place_id": place_id}, {'review_id': 1})
+    existing_list = [str(review['review_id']) for review in existing_review_id]
+    for review_id in new_reviews_id:
+        if review_id not in existing_list:
+            print("does if to check if changed")
+            return True
+
 def return_values(place_id):
     return {
         "place": json.loads(json_util.dumps(db['place'].find({"place_id": place_id}, {"_id": 0}))),
@@ -12,19 +20,8 @@ def return_values(place_id):
                                                                 {"_id": 0, "entities_score._id": 0}))),
     }
 
-
-def get_place_reviews(api_provider: str, place_id: str, review_count: int):
-    place = get_foursquare_place(place_id)
-    place_reviews = get_foursquare_place_reviews(place_id, review_count)
+def sentiment_analysis(place_id, place, place_reviews):
     reviews = []
-
-    search = db['place'].find_one({"place_id": place_id})
-    if search:
-        return return_values(place_id)
-    else:
-        place.update({"api_provider": api_provider})
-        db['place'].insert_one(place)
-
     for review in place_reviews:
         json_response = analyze_sentiment(review.get("text"))
         review_analyzed = {
@@ -50,4 +47,27 @@ def get_place_reviews(api_provider: str, place_id: str, review_count: int):
     db['review'].insert_many(reviews)
     db['place_reviews'].insert_one({"place": place, "reviews": reviews})
 
+
+def get_place_reviews(api_provider: str, place_id: str, review_count: int):
+    place = get_foursquare_place(place_id)
+    place_reviews = get_foursquare_place_reviews(place_id, review_count)
+
+    search = db['place'].find_one({"place_id": place_id})
+    if search:
+        # here, additional check to see if reviews have changed
+        # check list of reviewIDs if same in db as incoming
+        print("went in first if")
+        new_reviews = []
+        for review in place_reviews:
+            new_reviews.append(review.get("review_id"))
+        if have_reviews_changed(place_id, new_reviews) is False:
+            print(have_reviews_changed(place_id, new_reviews))
+            return return_values(place_id)
+            # do sentiment analysis
+    else:
+        print("went in first else")
+        place.update({"api_provider": api_provider})
+        db['place'].insert_one(place)
+
+    sentiment_analysis(place_id, place, place_reviews)
     return return_values(place_id)
