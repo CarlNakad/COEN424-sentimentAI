0fa1c4352f699f1a445a53520342e485935cec95
bashar9889 <bws9889@gmail.com>
2024-11-09 18:24:16 -0500

    hash password and verify it in authentication

--- app/auth.py
+++ app/auth.py
@@ -3,14 +3,29 @@ from datetime import datetime, timedelta
 from typing import Union
 from jose import JWTError, jwt
 from pydantic import BaseModel
+from passlib.context import CryptContext
 
 SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
 class TokenData(BaseModel):
     username: Union[str, None] = None
 
+
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+
+    
 def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None):
     to_encode = data.copy()
     if expires_delta:
