a260de6470ac63917eebb4010d14bbcc576b3271
carl1235 <carl.nakad@gmail.com>
2024-11-11 19:17:16 -0500

    Add error handling for invalid api_provider and review_count

--- app/place_review_api.py
+++ app/place_review_api.py
@@ -1,8 +1,13 @@
 import json
 from bson import json_util
+from fastapi import HTTPException
+
 from foursquare_api import get_foursquare_place, get_foursquare_place_reviews
 from google_nlp import analyze_sentiment
 from mongodb_connection import db
+import logging
+
+api_providers = ["foursquare"]
 
 def return_values(place_id, review_count):
     return {
@@ -16,32 +21,39 @@ def sentiment_analysis(place_id, place, place_reviews):
     reviews = []
     for review in place_reviews:
         json_response = analyze_sentiment(review.get("text"))
+        entities_score = json_response.get("entities_score")
         review_analyzed = {
             "place_id": place_id,
             "review_id": review.get("review_id"),
             "text": review.get("text"),
             "created_at": review.get("created_at"),
             "sentiment_score": json_response.get("sentiment_score"),
-            "entities_score": json_response.get("entities_score"),
+            "entities_score": entities_score,
             "language": json_response.get("language")
         }
-        entities_score = json_response.get("entities_score")
+
         for entity in entities_score:
             entity.update({"review_id": review.get("review_id")})
             entity.update({"place_id": place_id})
         db['entity_score'].insert_one({"entities_score": entities_score})
 
-        # for the review table
         reviews.append(review_analyzed)
-        # for the place_review table
         review.update({"reviews": review_analyzed})
 
-    db['review'].insert_many(reviews)
-    db['place_reviews'].insert_one({"place": place, "reviews": reviews})
+    if len(reviews) > 0:
+        db['review'].insert_many(reviews)
+        db['place_reviews'].insert_one({"place": place, "reviews": reviews})
 
 
 def get_place_reviews(api_provider: str, place_id: str, review_count: int):
+    if not api_provider or api_provider not in api_providers:
+        raise HTTPException(status_code=400, detail="Invalid API provider")
+    if review_count < 1:
+        raise HTTPException(status_code=400, detail="Invalid review count")
+
+
     place = get_foursquare_place(place_id)
+
     place_reviews = get_foursquare_place_reviews(place_id, review_count)
     place_reviews = sorted(place_reviews, key=lambda x: x.get("created_at"), reverse=True)
 
@@ -50,7 +62,6 @@ def get_place_reviews(api_provider: str, place_id: str, review_count: int):
     
     if db_reviews:
         found_new_reviews = db_reviews[0].get("review_id") != place_reviews[0].get("review_id")
-        print("found_new_reviews: ", found_new_reviews)
         if found_new_reviews or len(db_reviews) < review_count:
             sentiment_analysis(place_id, place, place_reviews[len(db_reviews):])
     else:
